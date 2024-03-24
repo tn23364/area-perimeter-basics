6153af6a28e1f1d28a2813244533b64fc47b41d8
tn23364 <164087727+tn23364@users.noreply.github.com>
2024-03-25 12:29:11 +1300

    variables

---
+++ P_02_variables.py
@@ -0,0 +1,12 @@
+# Code to learn about variables
+
+# variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# variables can be a number
+fav_num = 7
+num_string = "5"
+
+# constants are uppercase
+PI = 3.14
+a_01_my_num = 7
